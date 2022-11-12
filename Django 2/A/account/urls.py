5e29ac55d830772197f05964973eab46e2837d69
mehdi-mirzaie78 <mehdimirzaie1378@gmail.com>
2022-11-12 19:16:01 +0330

    Session 10 - login user with username and password

--- Django 2/A/account/urls.py
+++ Django 2/A/account/urls.py
@@ -5,4 +5,5 @@ from . import views
 app_name = 'account'
 urlpatterns = [
     path('register/', views.RegisterView.as_view(), name='register'),
+    path('login/', views.LoginView.as_view(), name='login'),
 ]
--- Django 2/A/account/views.py
+++ Django 2/A/account/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, redirect
 from django.views import View
-from .forms import UserRegistrationForm
+from .forms import UserRegistrationForm, UserLoginForm
 from django.contrib.auth.models import User
 from django.contrib import messages
+from django.contrib.auth import login, authenticate
 
 
 class RegisterView(View):
@@ -24,3 +25,25 @@ class RegisterView(View):
             messages.success(request, 'You registered successfully', 'success')
             return redirect('home:home')
         return render(request, self.template_name, {'form': form})
+
+
+class LoginView(View):
+    class_form = UserLoginForm
+    template_name = 'account/login.html'
+
+    def get(self, request):
+        form = self.class_form()
+        return render(request, self.template_name, {'form': form})
+
+    def post(self, request):
+        form = self.class_form(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            user = authenticate(request, username=cd['username'], password=cd['password'])
+            if user is not None:
+                login(request, user)
+                messages.success(request, 'You logged in successfully', 'success')
+                return redirect('home:home')
+            messages.error(request, 'Username or Password is WRONG', 'danger')
+        return render(request, self.template_name, {'form': form})
+
