0b9627403414bb5f263e617e54cceb6603d254d2
mehdi-mirzaie78 <mehdimirzaie1378@gmail.com>
2022-11-12 14:30:50 +0330

    Finished the sessions: 5, 6, 7

--- Django 2/A/account/forms.py
+++ Django 2/A/account/forms.py
@@ -2,8 +2,6 @@ from django import forms
 
 
 class UserRegistrationForm(forms.Form):
-    username = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
-    email = forms.EmailField(widget=forms.EmailInput(attrs={'class': 'form-control'}))
-    password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control'}))
-
-
+    username = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control mt-2', 'placeholder': 'Your username'}))
+    email = forms.EmailField(widget=forms.EmailInput(attrs={'class': 'form-control mt-2', 'placeholder': 'sample@gmail.com'}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control mt-2', 'placeholder': 'Your password'}))
