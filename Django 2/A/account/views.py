15814d4f740b1fee6c691f261e5d75e416ed4fe6
mehdi-mirzaie78 <mehdimirzaie1378@gmail.com>
2022-11-13 07:09:00 +0330

    Session 13: LoginRequiredMixin for logout view

--- Django 2/A/account/views.py
+++ Django 2/A/account/views.py
@@ -4,6 +4,7 @@ from .forms import UserRegistrationForm, UserLoginForm
 from django.contrib.auth.models import User
 from django.contrib import messages
 from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
 class UserRegisterView(View):
@@ -58,7 +59,7 @@ class UserLoginView(View):
         return render(request, self.template_name, {'form': form})
 
 
-class UserLogoutView(View):
+class UserLogoutView(LoginRequiredMixin, View):
     def get(self, request):
         logout(request)
         messages.success(request, 'You logged out successfully', 'success')
