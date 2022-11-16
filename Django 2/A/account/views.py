b59dfd0fa74992779151fea3df8dca677b9cee43
mehdi-mirzaie78 <mehdimirzaie1378@gmail.com>
2022-11-16 09:30:30 +0330

    Session 33: Next | Redirect to next url

--- Django 2/A/account/views.py
+++ Django 2/A/account/views.py
@@ -40,6 +40,10 @@ class UserLoginView(View):
     class_form = UserLoginForm
     template_name = 'account/login.html'
 
+    def setup(self, request, *args, **kwargs):
+        self.next = request.GET.get('next')
+        return super().setup(request, *args, **kwargs)
+
     def dispatch(self, request, *args, **kwargs):
         if request.user.is_authenticated:
             return redirect('home:home')
@@ -57,6 +61,8 @@ class UserLoginView(View):
             if user is not None:
                 login(request, user)
                 messages.success(request, 'You logged in successfully', 'success')
+                if self.next:
+                    return redirect(self.next)
                 return redirect('home:home')
             messages.error(request, 'Username or Password is WRONG', 'danger')
         return render(request, self.template_name, {'form': form})
