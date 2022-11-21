4eb0c35a5734843c1278a498fd446a49d9b2f3aa
mehdi-mirzaie78 <mehdimirzaie1378@gmail.com>
2022-11-21 15:13:14 +0330

    Session 41: Error

--- Django 2/A/home/models.py
+++ Django 2/A/home/models.py
@@ -5,6 +5,7 @@ from django.urls import reverse
 
 class Post(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
+    title = models.CharField(max_length=100, null=True, blank=True)
     body = models.TextField()
     slug = models.SlugField()
     created = models.DateTimeField(auto_now_add=True)
