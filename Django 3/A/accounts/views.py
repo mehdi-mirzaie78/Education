b8bed0d2eaa612148ddbc7e2463f51cb441b8530
mehdi-mirzaie78 <mehdimirzaie1378@gmail.com>
2022-11-23 19:34:19 +0330

    Django shop | Session 7 | Session

--- Django 3/A/accounts/views.py
+++ Django 3/A/accounts/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
 from .forms import UserRegistrationForm
+import random
+from utils import send_top_code
+from .models import OtpCode
+from django.contrib import messages
 
 
 class UserRegisterView(View):
@@ -9,6 +13,28 @@ class UserRegisterView(View):
     def get(self, request):
         form = self.form_class
         return render(request, 'accounts/register.html', {'form': form})
+
+    def post(self, request):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            random_code = random.randint(1000, 9999)
+            send_top_code(cd['phone'], random_code)
+            OtpCode.objects.create(phone_number=cd['phone'], code=random_code)
+            request.session['user_registration_info'] = {
+                'phone_number': cd['phone'],
+                'email': cd['email'],
+                'full_name': cd['full_name'],
+                'password': cd['password'],
+            }
+            messages.success(request, 'We sent you a code', 'success')
+            return redirect('accounts:verify_code')
+        return redirect('home:home')
+
+
+class UserRegisterVerifyCodeView(View):
+    def get(self, request):
+        pass
 
     def post(self, request):
         pass
