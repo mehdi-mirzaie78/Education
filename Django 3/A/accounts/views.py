9d349c8acdb9a4131d986a9ed73fb328a73f7bed
mehdi-mirzaie78 <mehdimirzaie1378@gmail.com>
2022-11-23 20:14:28 +0330

    Django shop | Session 8 | Verify code

--- Django 3/A/accounts/views.py
+++ Django 3/A/accounts/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render, redirect
 from django.views import View
-from .forms import UserRegistrationForm
+from .forms import UserRegistrationForm, VerifyCodeForm
 import random
 from utils import send_top_code
-from .models import OtpCode
+from .models import OtpCode, User
 from django.contrib import messages
 
 
@@ -33,8 +33,26 @@ class UserRegisterView(View):
 
 
 class UserRegisterVerifyCodeView(View):
+    form_class = VerifyCodeForm
+
     def get(self, request):
-        pass
+        form = self.form_class
+        return render(request, 'accounts/verify.html', {'form': form})
 
     def post(self, request):
-        pass
+        user_session = request.session['user_registration_info']
+        code_instance = OtpCode.objects.get(phone_number=user_session['phone_number'])
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            if cd['code'] == code_instance.code:
+                User.objects.create_user(user_session['phone_number'], user_session['email'],
+                                         user_session['full_name'], user_session['password'])
+                code_instance.delete()
+                messages.success(request, 'You registered successfully.', 'success')
+                return redirect('home:home')
+            else:
+                messages.error(request, 'This code is WRONG', 'danger')
+                return redirect('accounts:verify_code')
+        return redirect('home:home')
+
