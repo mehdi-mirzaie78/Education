e90eb222c209c1dc69de5726c1efa9582e36dc43
mehdi-mirzaie78 <mehdimirzaie1378@gmail.com>
2022-11-25 19:58:34 +0330

    Django shop | Session 13 | Product detail

--- Django 3/A/A/urls.py
+++ Django 3/A/A/urls.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls', namespace='home')),
     path('accounts/', include('accounts.urls', namespace='accounts')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+# static(settings.MEDIA.URL, document_root=settings.MEDIA_ROOT) is only for development mode
+# You can't use this on production mode
