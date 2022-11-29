067ac03a7376d792af617673dc0dd5f08558d043
mehdi-mirzaie78 <mehdimirzaie1378@gmail.com>
2022-11-30 02:16:00 +0330

    Django shop | Session 21 | Naive and Aware datetime

--- Django 3/A/accounts/forms.py
+++ Django 3/A/accounts/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import User
+from .models import User, OtpCode
 from django.core.exceptions import ValidationError
 from django.contrib.auth.forms import ReadOnlyPasswordHashField
 
@@ -54,6 +54,7 @@ class UserRegistrationForm(forms.Form):
         user = User.objects.filter(phone_number=phone)
         if user.exists():
             raise ValidationError('This Phone number is already exists')
+        OtpCode.objects.filter(phone_number=phone).delete()
         return phone
 
 
