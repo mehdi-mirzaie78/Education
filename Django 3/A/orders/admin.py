bb86e3ab2ed393e8d9167ad4287aa124b2417504
mehdi-mirzaie78 <mehdimirzaie1378@gmail.com>
2022-12-03 18:58:24 +0330

    Django shop | Session 37 | Coupon session 35, 36 is about zarinpal

--- Django 3/A/orders/admin.py
+++ Django 3/A/orders/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Order, OrderItem
+from .models import Order, OrderItem, Coupon
 
 
 class OrderItemInline(admin.TabularInline):
@@ -12,3 +12,6 @@ class OrderAdmin(admin.ModelAdmin):
     list_display = ('id', 'user', 'updated', 'paid')
     list_filter = ('paid', 'user')
     inlines = (OrderItemInline,)
+
+
+admin.site.register(Coupon)
--- Django 3/A/orders/forms.py
+++ Django 3/A/orders/forms.py
@@ -5,3 +5,5 @@ class CartAddForm(forms.Form):
     quantity = forms.IntegerField(min_value=1, max_value=9)
 
 
+class CouponApplyForm(forms.Form):
+    code = forms.CharField(label='Discount code')
