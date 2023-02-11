952519003edb74ffed77e25b524e25adab80c427
mehdi-mirzaie78 <mehdimirzaie1378@gmail.com>
2023-02-11 23:20:58 +0330

    Fastapi | main.py | Added create_table for connection between fast and postgres

--- FastAPI/backend/main.py
+++ FastAPI/backend/main.py
@@ -2,6 +2,9 @@ from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from core.config import settings
 from apis.general_pages.route_homepage import general_pages_router
+from db.session import engine
+from db.base_class import Base
+
 # import uvicorn
 
 
@@ -12,10 +15,14 @@ def include_router(app):
 def configure_static(app):
     app.mount("/static", StaticFiles(directory="static"), name="static")
 
+def create_tables():
+    Base.metadata.create_all(bind=engine)
+
 def start_application():
     app = FastAPI(title=settings.PROJECT_NAME, version=settings.PROJECT_VERSION)
     include_router(app)
     configure_static(app)
+    create_tables()
     return app
 
 
