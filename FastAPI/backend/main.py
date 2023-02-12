c54412efca8f918c12f0323b7bff0caf3f68e552
mehdi-mirzaie78 <mehdimirzaie1378@gmail.com>
2023-02-12 23:51:40 +0330

    fastapi | main.py | Modified

--- FastAPI/backend/main.py
+++ FastAPI/backend/main.py
@@ -4,7 +4,6 @@ from core.config import settings
 from apis.general_pages.route_homepage import general_pages_router
 from db.session import engine
 from db.base import Base
-
 # import uvicorn
 
 
@@ -15,8 +14,10 @@ def include_router(app):
 def configure_static(app):
     app.mount("/static", StaticFiles(directory="static"), name="static")
 
+
 def create_tables():
     Base.metadata.create_all(bind=engine)
+
 
 def start_application():
     app = FastAPI(title=settings.PROJECT_NAME, version=settings.PROJECT_VERSION)
@@ -29,19 +30,8 @@ def start_application():
 app = start_application()
 
 # if __name__ == "__main__":
-    # uvicorn.run(app, host="127.0.0.1", port=8000, reload=False)
+# uvicorn.run(app, host="127.0.0.1", port=8000, reload=False)
 
 
 # You can run this in the terminal if you want to:
 # uvicorn main:app --reload
-
-
-
-
-
-
-
-
-
-
-
